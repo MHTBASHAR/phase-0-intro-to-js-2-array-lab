3ac4fee23b86fa15895ddc8db9bf8a3dab652bc3
Mahmat Bachar <ummahamat23@gmail.com>
2025-02-07 13:36:30 -0500

    completed Arrays Lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"];
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+function  destructivelyAppendCat(name){
+    return cats.push(name)
+}
+function  destructivelyRemoveFirstCat(){
+    return cats.shift()
+}
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+function appendCat(name){
+    return [...cats, name];
+}
+function prependCat(name){
+    return [name, ...cats];
+}
+function removeLastCat(){
+    return cats.slice(0, 2);
+}
+function removeFirstCat(){
+    return cats.slice(1, cats.length);
+}
